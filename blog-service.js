8f3226e502c2945c42e4e1081c16518f5b534b01
kasrarh <55912648+kasrarh@users.noreply.github.com>
2023-06-14 12:39:36 -0400

    assignment 3

--- blog-service.js
+++ blog-service.js
@@ -1,4 +1,5 @@
 const fs = require("fs");
+const posts = [];
 
 const getAllPosts = () => {
   return new Promise((resolve, reject) => {
@@ -39,8 +40,57 @@ const getPublishedPosts = () => {
   });
 };
 
+function addPost(postData) {
+  return new Promise((resolve, reject) => {
+    postData.published = postData.published ? true : false;
+    postData.id = posts.length + 1;
+    posts.push(postData);
+    resolve(postData);
+  });
+}
+
+function getPostsByCategory(category) {
+  return new Promise((resolve, reject) => {
+    const filteredPosts = posts.filter((post) => post.category === parseInt(category));
+
+    if (filteredPosts.length > 0) {
+      resolve(filteredPosts);
+    } else {
+      reject("No results returned");
+    }
+  });
+}
+function getPostsByMinDate(minDateStr) {
+  return new Promise((resolve, reject) => {
+    const filteredPosts = posts.filter((post) => new Date(post.postDate) >= new Date(minDateStr));
+
+    if (filteredPosts.length > 0) {
+      resolve(filteredPosts);
+    } else {
+      reject("No results returned");
+    }
+  });
+}
+
+function getPostById(id) {
+  return new Promise((resolve, reject) => {
+    const post = posts.find((post) => post.id === id);
+
+    if (post) {
+      resolve(post);
+    } else {
+      reject("No result returned");
+    }
+  });
+}
+
 module.exports = {
   getAllPosts,
   getCategories,
   getPublishedPosts,
+  addPost,
+  getPostsByCategory,
+  getPostsByMinDate,
+  getPostById,
+  
 };
--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 /*********************************************************************************
-*  WEB322 – Assignment 02
+*  WEB322 – Assignment 03
 *  I declare that this assignment is my own work in accordance with Seneca  Academic Policy.  No part *  of this assignment has been copied manually or electronically from any other source 
 *  (including 3rd party web sites) or distributed to other students.
 * 
-*  Name: Arzu Gizem Kirici Student ID: 135304210 Date: May 30,2023
+*  Name: Arzu Gizem Kirici Student ID: 135304210 Date: Jun 14,2023
 *
 *  Cyclic Web App URL: https://agile-veil-hare.cyclic.app
 *
@@ -18,6 +18,21 @@ const app = epxress();
 
 const path = require("path");
 
+const multer = require("multer");
+const cloudinary = require('cloudinary').v2
+const streamifier = require('streamifier')
+
+
+cloudinary.config({ 
+  cloud_name: 'dkl3nddi4', 
+  api_key: '527131747989354', 
+  api_secret: 'NW3e805bo7c9fxCc-p914HAFdwk' ,
+  secure: true
+});
+
+const upload = multer(); // no { storage: storage } 
+
+
 console.log(__dirname);
 
 app.use(epxress.static("./public"));
@@ -27,14 +42,42 @@ app.get("/", (request, response) => {
 });
 
 app.get("/posts", (req, res) => {
-  blogService
-    .getAllPosts()
-    .then((data) => {
-      res.send(JSON.parse(data));
-    })
-    .catch((err) => {
-      res.send(err.message);
-    });
+  const { category, minDate } = req.query;
+
+  if (category) {
+    // If category is present in the query parameters, filter posts by category
+    const filteredPosts = blogService.getPostsByCategory(category);
+    res.json(filteredPosts);
+  } else if (minDate) {
+    // If minDate is present in the query parameters, filter posts by minimum date
+    const filteredPosts = blogService.getPostsByMinDate(minDate);
+    res.json(filteredPosts);
+  } else {
+    // No filters specified, return all posts
+    const allPosts = blogService.getAllPosts();
+    res.json(allPosts);
+  }
+  // blogService
+  //   .getAllPosts()
+  //   .then((data) => {
+  //     res.send(JSON.parse(data));
+  //   })
+  //   .catch((err) => {
+  //     res.send(err.message);
+  //   });
+});
+
+app.get('/post/:id', (req, res) => {
+  const postId = parseInt(req.params.id);
+
+  // Call the getPostById function from the blog service to retrieve the post by id
+  const post = blogService.getPostById(postId);
+
+  if (post) {
+    res.json(post);
+  } else {
+    res.status(404).json({ error: 'Post not found' });
+  }
 });
 
 app.get("/blog", (req, res) => {
@@ -60,6 +103,66 @@ app.get("/categories", (req, res) => {
 
 app.get("/about", (req, res) => {
   res.sendFile(path.join(__dirname, "./views/about.html"));
+});
+
+app.get('/posts/add', (req, res) => {
+  res.sendFile(path.join(__dirname, 'views', 'addPost.html'));
+});
+
+app.post('/posts/add', upload.single('featureImage'), (req, res) => {
+  if (req.file) {
+    let streamUpload = (req) => {
+      return new Promise((resolve, reject) => {
+        let stream = cloudinary.uploader.upload_stream((error, result) => {
+          if (result) {
+            resolve(result);
+          } else {
+            reject(error);
+          }
+        });
+
+        streamifier.createReadStream(req.file.buffer).pipe(stream);
+      });
+    };
+
+    async function upload(req) {
+      let result = await streamUpload(req);
+      console.log(result);
+      return result;
+    }
+
+    upload(req)
+      .then((uploaded) => {
+        processPost(uploaded.url);
+      })
+      .catch((error) => {
+        console.error(error);
+        processPost('');
+      });
+  } else {
+    processPost('');
+  }
+  function processPost(imageUrl) {
+    req.body.featureImage = imageUrl;
+
+    const newPostData = {
+      title: req.body.title,
+      content: req.body.content,
+      category: req.body.category,
+      published: req.body.published
+    };
+
+    // Call the addPost function from the blog service to add the new blog post
+    blogService.addPost(newPostData)
+      .then((addedPostData) => {
+        console.log('New blog post added:', addedPostData);
+        res.redirect('/posts');
+      })
+      .catch((error) => {
+        console.error('Failed to add new blog post:', error);
+        res.redirect('/posts/add');
+      });
+  }
 });
 
 app.all("*", (req, res) => {
